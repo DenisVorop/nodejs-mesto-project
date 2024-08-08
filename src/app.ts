909b9625856ffc9ca0e1abc48b9939a2fe070bd8
DenisVorop <saskeazaske@icloud.com>
2024-08-08 23:48:05 +0300

    init mongodb

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,27 @@
+// src/app.ts
+
 import express, { Request, Response } from "express";
+import mongoose from "mongoose";
 import dotenv from "dotenv";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+const MONGODB_URL =
+  process.env.MONGODB_URL || "mongodb://localhost:27017/mestodb";
+
+mongoose
+  .connect(MONGODB_URL, {})
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((error) => {
+    console.error("Error connecting to MongoDB:", error);
+  });
 
 app.get("/", (req: Request, res: Response) => {
-  res.send("Hello, TypeScript with Express!");
+  res.send("Hello, TypeScript with Express and MongoDB!");
 });
 
 app.listen(PORT, () => {
