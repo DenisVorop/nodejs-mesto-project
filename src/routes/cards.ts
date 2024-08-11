54dc3d7c24366af4343d1e1fdbda44284a36e571
DenisVorop <saskeazaske@icloud.com>
2024-08-11 21:45:39 +0300

    feat: protect `cards` & `users` routes

--- src/routes/cards.ts
+++ src/routes/cards.ts
@@ -6,13 +6,14 @@ import {
   likeCard,
   dislikeCard,
 } from "../controllers/cards";
+import auth from "middleware/auth";
 
 const router = Router();
 
-router.get("/cards", getCards);
-router.post("/cards", createCard);
-router.delete("/cards/:cardId", deleteCard);
-router.put("/cards/:cardId/likes", likeCard);
-router.delete("/cards/:cardId/likes", dislikeCard);
+router.get("/cards", auth, getCards);
+router.post("/cards", auth, createCard);
+router.delete("/cards/:cardId", auth, deleteCard);
+router.put("/cards/:cardId/likes", auth, likeCard);
+router.delete("/cards/:cardId/likes", auth, dislikeCard);
 
 export default router;
--- src/routes/users.ts
+++ src/routes/users.ts
@@ -6,12 +6,13 @@ import {
   updateProfile,
   updateAvatar,
 } from "../controllers/users";
+import auth from "middleware/auth";
 
 const router = Router();
 
-router.get("/users", getUsers);
-router.get("/users/:userId", getUserById);
-router.patch("/users/me", updateProfile);
-router.patch("/users/me/avatar", updateAvatar);
+router.get("/users", auth, getUsers);
+router.get("/users/:userId", auth, getUserById);
+router.patch("/users/me", auth, updateProfile);
+router.patch("/users/me/avatar", auth, updateAvatar);
 
 export default router;
