bdbda597472926d39d183a454010105231c421a9
DenisVorop <saskeazaske@icloud.com>
2024-08-11 21:15:22 +0300

    feat: add `email` & `password` to `User` model

--- src/models/User.ts
+++ src/models/User.ts
@@ -12,23 +12,24 @@ export interface IUser extends Document {
 const userSchema = new mongoose.Schema<IUser>({
   name: {
     type: String,
-    required: true,
     minlength: 2,
     maxlength: 30,
+    default: "Жак-Ив Кусто",
   },
   about: {
     type: String,
-    required: true,
     minlength: 2,
     maxlength: 200,
+    default: "Исследователь",
   },
   avatar: {
     type: String,
-    required: true,
     validate: {
       validator: (v: string) => validator.isURL(v),
       message: "Некорректная ссылка на аватар",
     },
+    default:
+      "https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png",
   },
   email: {
     type: String,
@@ -42,6 +43,7 @@ const userSchema = new mongoose.Schema<IUser>({
   password: {
     type: String,
     required: true,
+    select: false,
   },
 });
 
