647977dbc633b69d398a2bbcf7ed02e64b790f25
DenisVorop <saskeazaske@icloud.com>
2024-08-11 21:21:42 +0300

    feat: add `login` controller

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -2,6 +2,50 @@ import { Request, Response } from "express";
 import User from "../models/User";
 import mongoose from "mongoose";
 import bcrypt from "bcryptjs";
+import dotenv from "dotenv";
+import jwt, { type Secret } from "jsonwebtoken";
+
+dotenv.config();
+
+const { JWT_SECRET, JWT_EXPIRES_IN } = process.env;
+
+export const login = async (req: Request, res: Response): Promise<void> => {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      res.status(400).json({ message: "Email и пароль обязательны" });
+      return;
+    }
+
+    const user = await User.findOne({ email }).select("+password");
+    if (!user) {
+      res.status(401).json({ message: "Неправильные почта или пароль" });
+      return;
+    }
+
+    const isPasswordCorrect = await bcrypt.compare(password, user.password);
+    if (!isPasswordCorrect) {
+      res.status(401).json({ message: "Неправильные почта или пароль" });
+      return;
+    }
+
+    const token = jwt.sign({ _id: user._id }, JWT_SECRET as Secret, {
+      expiresIn: JWT_EXPIRES_IN,
+    });
+
+    res
+      .status(200)
+      .cookie("jwt", token, {
+        httpOnly: true,
+        secure: true,
+        maxAge: 7 * 24 * 60 * 60 * 1000,
+      })
+      .json({ message: "Аутентификация успешна" });
+  } catch (err: unknown) {
+    res.status(500).json({ message: "Непредвиденная ошибка сервера" });
+  }
+};
 
 export const getUsers = async (req: Request, res: Response): Promise<void> => {
   try {
